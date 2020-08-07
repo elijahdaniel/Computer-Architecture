8ca8641c09a4e5733ef9eb5839f486d8e54940eb
elijahdaniel <e.paminsan@gmail.com>
2020-08-06 20:23:18 -0700

    mvp for day 1

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,12 +2,27 @@
 
 import sys
 
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = [0] * 256
+
+        self.registers = [0] * 8
+        self.pc = 0
+        self.fl = 0
+        # commands
+        self.HLT = 0b00000001
+        self.LDI = 0b10000010
+        self.PRN = 0b01000111
+
+    def ram_read(self, index):
+        return self.ram[index]
+
+    def ram_write(self, value, index):
+        self.ram[index] = value
 
     def load(self):
         """Load a program into memory."""
@@ -18,25 +33,24 @@ class CPU:
 
         program = [
             # From print8.ls8
-            0b10000010, # LDI R0,8
+            0b10000010,  # LDI R0,8
             0b00000000,
             0b00001000,
-            0b01000111, # PRN R0
+            0b01000111,  # PRN R0
             0b00000000,
-            0b00000001, # HLT
+            0b00000001,  # HLT
         ]
 
         for instruction in program:
             self.ram[address] = instruction
             address += 1
 
-
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -48,8 +62,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -62,4 +76,27 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        running = True
+        while running:
+
+            ir = self.ram_read(self.pc)
+
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+
+            if ir == self.LDI:
+
+                reg_index = self.ram[self.pc + 1]
+
+                value_to_save = self.ram[self.pc + 2]
+                self.registers[reg_index] = value_to_save
+
+                self.pc += 2
+            if ir == self.PRN:
+                reg_index = self.ram[self.pc+1]
+                print(self.registers[reg_index])
+                self.pc += 1
+            if ir == self.HLT:
+                running = False
+
+            self.pc += 1
