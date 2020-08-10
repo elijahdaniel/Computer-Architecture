c85bc6cf01fc7fd87227a4b34bbe816ec130d3b8
elijahdaniel <e.paminsan@gmail.com>
2020-08-10 14:30:49 -0700

    day 2

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -9,17 +9,20 @@ class CPU:
     def __init__(self):
         """Construct a new CPU."""
         self.ram = [0] * 256
-
         self.registers = [0] * 8
-        self.pc = 0
-        self.fl = 0
-        # commands
-        self.HLT = 0b00000001
-        self.LDI = 0b10000010
-        self.PRN = 0b01000111
+        self.pc = 0  # program counter
+        self.fl = 0  # flags status
+        self.ir = 0  # instruction register
+
+        # Commands
+        self.HLT = 0b00000001  # HALT, exit the emulator
+        self.LDI = 0b10000010  # LOAD, sets a value to a specified register
+        self.PRN = 0b01000111  # PRINT, prints value of a register
+        self.MUL = 0b10100010  # MULTIPLY
 
     def ram_read(self, index):
         return self.ram[index]
+    # should accept a value to write, and the address to write it to.
 
     def ram_write(self, value, index):
         self.ram[index] = value
@@ -28,18 +31,28 @@ class CPU:
         """Load a program into memory."""
 
         address = 0
+        program = []
 
-        # For now, we've just hardcoded a program:
+        if len(sys.argv) < 2:
+            print("Please pass in a second file.")
+            sys.exit()
 
-        program = [
-            # From print8.ls8
-            0b10000010,  # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111,  # PRN R0
-            0b00000000,
-            0b00000001,  # HLT
-        ]
+        file_name = sys.argv[1]
+
+        try:
+            with open(file_name) as file:
+                for line in file:
+                    split_line = line.split('#')[0]
+                    command = split_line.strip()
+
+                    if command == '':
+                        continue
+                    # after factoring append whatever is left to the program array
+                    program.append(int(command, 2))
+
+        except FileNotFoundError:
+            print(f'{sys.argv[0]}: {sys.argv[1]} file was not found')
+            sys.exit()
 
         for instruction in program:
             self.ram[address] = instruction
@@ -50,7 +63,8 @@ class CPU:
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        # elif op == "SUB": etc
+        if op == "MUL":
+            self.registers[reg_a] *= self.registers[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -62,8 +76,6 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            # self.fl,
-            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -78,24 +90,33 @@ class CPU:
         """Run the CPU."""
         running = True
         while running:
-
+            # read memory at pc, set to instruction register
+            # this is the same as the memory[pc] array [PRINT_TIM, PRINT_TIM, HALT]
             ir = self.ram_read(self.pc)
 
+            # look at the following 1 or 2 bytes of memory, stored as variables..why exactly??
             operand_a = self.ram_read(self.pc + 1)
             operand_b = self.ram_read(self.pc + 2)
 
+            # if command = LDI
             if ir == self.LDI:
-
+                # grab the memory address 1 past the command, here the value is the spot in ram to save the value
+                #   = memory[pc + 1]
                 reg_index = self.ram[self.pc + 1]
-
+                # Grab value 2 pass the command, this value will get stored in the ram slot above
                 value_to_save = self.ram[self.pc + 2]
                 self.registers[reg_index] = value_to_save
-
+                # increment program counter enough to pass the command + the slot + the value
+                self.pc += 2
+            if ir == self.MUL:
+                self.registers[self.ram_read(
+                    self.pc + 1)] *= self.registers[self.ram_read(self.pc + 2)]
                 self.pc += 2
             if ir == self.PRN:
                 reg_index = self.ram[self.pc+1]
                 print(self.registers[reg_index])
                 self.pc += 1
+
             if ir == self.HLT:
                 running = False
 
