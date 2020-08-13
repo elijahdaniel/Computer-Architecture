52e5fafac9436f50f6fb8a4fdbd10d72936b06a3
elijahdaniel <e.paminsan@gmail.com>
2020-08-13 09:45:58 -0700

    day 3

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -8,20 +8,67 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
+        self.running = True
         self.ram = [0] * 256
         self.registers = [0] * 8
+        self.registers[7] = 0xF4
         self.pc = 0  # program counter
         self.fl = 0  # flags status
         self.ir = 0  # instruction register
 
         # Commands
-        self.HLT = 0b00000001  # HALT, exit the emulator
-        self.LDI = 0b10000010  # LOAD, sets a value to a specified register
-        self.PRN = 0b01000111  # PRINT, prints value of a register
-        self.MUL = 0b10100010  # MULTIPLY
+        self.branchtable = {}
+        self.branchtable[0b00000001] = self.HLT
+        self.branchtable[0b10000010] = self.LDI
+        self.branchtable[0b01000111] = self.PRN
+        self.branchtable[0b10100010] = self.MUL
+        self.branchtable[0b01000101] = self.PUSH
+        self.branchtable[0b01000110] = self.POP
+
+        # self.HLT = 0b00000001  # HALT, exit the emulator
+        # self.LDI = 0b10000010  # LOAD, sets a value to a specified register
+        # self.PRN = 0b01000111  # PRINT, prints value of a register
+        # self.MUL = 0b10100010  # MULTIPLY
+
+    def HLT(self):
+        self.running = False
+
+    def LDI(self):
+        reg_index = self.ram[self.pc + 1]
+        value_to_save = self.ram[self.pc + 2]
+        self.registers[reg_index] = value_to_save
+        self.pc += 1 + (self.ir >> 6)
+
+    def PRN(self):
+        reg_index = self.ram[self.pc + 1]
+        print(self.registers[reg_index])
+        self.pc += 1 + (self.ir >> 6)
+
+    def MUL(self):
+        operand_a = self.ram_read(self.pc + 1)
+        operand_b = self.ram_read(self.pc + 2)
+        self.alu('MUL', operand_a, operand_b)
+        self.pc += 1 + (self.ir >> 6)
+
+    def PUSH(self):
+        self.registers[7] -= 1
+        sp = self.registers[7]
+        reg_index = self.ram[self.pc+1]
+        value = self.registers[reg_index]
+        self.ram[sp] = value
+        self.pc += 1 + (self.ir >> 6)
+
+    def POP(self):
+        sp = self.registers[7]
+        value = self.ram[sp]
+        reg_index = self.ram[self.pc + 1]
+        self.registers[reg_index] = value
+        self.registers[7] += 1
+        self.pc += 1 + (self.ir >> 6)
 
     def ram_read(self, index):
         return self.ram[index]
+
     # should accept a value to write, and the address to write it to.
 
     def ram_write(self, value, index):
@@ -88,36 +135,46 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        running = True
-        while running:
-            # read memory at pc, set to instruction register
-            # this is the same as the memory[pc] array [PRINT_TIM, PRINT_TIM, HALT]
-            ir = self.ram_read(self.pc)
+        while self.running:
+            self.ir = self.ram_read(self.pc)
 
-            # look at the following 1 or 2 bytes of memory, stored as variables..why exactly??
-            operand_a = self.ram_read(self.pc + 1)
-            operand_b = self.ram_read(self.pc + 2)
+            try:
+                self.branchtable[self.ir]()
 
-            # if command = LDI
-            if ir == self.LDI:
-                # grab the memory address 1 past the command, here the value is the spot in ram to save the value
-                #   = memory[pc + 1]
-                reg_index = self.ram[self.pc + 1]
-                # Grab value 2 pass the command, this value will get stored in the ram slot above
-                value_to_save = self.ram[self.pc + 2]
-                self.registers[reg_index] = value_to_save
-                # increment program counter enough to pass the command + the slot + the value
-                self.pc += 2
-            if ir == self.MUL:
-                self.registers[self.ram_read(
-                    self.pc + 1)] *= self.registers[self.ram_read(self.pc + 2)]
-                self.pc += 2
-            if ir == self.PRN:
-                reg_index = self.ram[self.pc+1]
-                print(self.registers[reg_index])
-                self.pc += 1
+            except KeyError:
+                print('Operation does not exist')
+                sys.exit()
 
-            if ir == self.HLT:
-                running = False
+        # running = True
+        # while running:
+        #     # read memory at pc, set to instruction register
+        #     # this is the same as the memory[pc] array [PRINT_TIM, PRINT_TIM, HALT]
+        #     ir = self.ram_read(self.pc)
 
-            self.pc += 1
+        #     # look at the following 1 or 2 bytes of memory, stored as variables..why exactly??
+        #     operand_a = self.ram_read(self.pc + 1)
+        #     operand_b = self.ram_read(self.pc + 2)
+
+        #     # if command = LDI
+        #     if ir == self.LDI:
+        #         # grab the memory address 1 past the command, here the value is the spot in ram to save the value
+        #         #   = memory[pc + 1]
+        #         reg_index = self.ram[self.pc + 1]
+        #         # Grab value 2 pass the command, this value will get stored in the ram slot above
+        #         value_to_save = self.ram[self.pc + 2]
+        #         self.registers[reg_index] = value_to_save
+        #         # increment program counter enough to pass the command + the slot + the value
+        #         self.pc += 2
+        #     if ir == self.MUL:
+        #         self.registers[self.ram_read(
+        #             self.pc + 1)] *= self.registers[self.ram_read(self.pc + 2)]
+        #         self.pc += 2
+        #     if ir == self.PRN:
+        #         reg_index = self.ram[self.pc+1]
+        #         print(self.registers[reg_index])
+        #         self.pc += 1
+
+        #     if ir == self.HLT:
+        #         running = False
+
+        #     self.pc += 1
